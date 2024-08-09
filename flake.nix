90875e4ef6d3ee344bbfdb56556fa03463918583
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2024-08-08 18:59:14 -0700

    fix: remove unrecognized attributes
    nonfree software will have to be enabled somewhere else

--- flake.nix
+++ flake.nix
@@ -40,7 +40,6 @@
   outputs = { self, nixpkgs, quasaros, config, ... }@inputs: {
     # Build the system
     nixosConfigurations.netsanet = (quasaros.make config).system;
-    nixpkgs.config.allowUnfree = true;
 
     formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-classic;
   };
